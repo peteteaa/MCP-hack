45faa22370fe11171c6d27d60848443762875e1e
nishoof <88177986+nishoof@users.noreply.github.com>
2025-05-02 14:46:09 -0700

    Add apify (twitter scraping)

---
+++ backend/apify.js
@@ -0,0 +1,53 @@
+import dotenv from "dotenv";
+import { ApifyClient } from "apify-client";
+
+dotenv.config();
+
+// Initialize the ApifyClient with API token
+const client = new ApifyClient({
+    token: process.env.APIFY_API_TOKEN,
+});
+
+// Prepare Actor input
+const input = {
+    "filter:blue_verified": false,
+    "filter:consumer_video": false,
+    "filter:has_engagement": false,
+    "filter:hashtags": false,
+    "filter:images": false,
+    "filter:links": false,
+    "filter:media": false,
+    "filter:mentions": false,
+    "filter:native_video": false,
+    "filter:nativeretweets": false,
+    "filter:news": false,
+    "filter:pro_video": false,
+    "filter:quote": false,
+    "filter:replies": false,
+    "filter:safe": false,
+    "filter:spaces": false,
+    "filter:twimg": false,
+    "filter:verified": false,
+    "filter:videos": false,
+    "filter:vine": false,
+    "from": "elonmusk",
+    "include:nativeretweets": false,
+    "lang": "en",
+    "searchTerms": [
+        "GTC 2025"
+    ],
+    "since": "2021-12-31_23:59:59_UTC",
+    "until": "2024-12-31_23:59:59_UTC"
+};
+
+(async () => {
+    // Run the Actor and wait for it to finish
+    const run = await client.actor("CJdippxWmn9uRfooo").call(input);
+
+    // Fetch and print Actor results from the run's dataset (if any)
+    console.log("Results from dataset");
+    const { items } = await client.dataset(run.defaultDatasetId).listItems();
+    items.forEach((item) => {
+        console.dir(item);
+    });
+})();
